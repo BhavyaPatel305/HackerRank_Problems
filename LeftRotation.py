cfbb7631448673bb76641afdb5a5134b9c23ba51
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-07-14 00:41:09 +0530

    Create LeftRotation.py
    A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. Given an integer, , rotate the array that many steps left and return the result.
    
    Example
    
    
    After  rotations, .
    
    Function Description
    
    Complete the rotateLeft function in the editor below.
    
    rotateLeft has the following parameters:
    
    int d: the amount to rotate by
    int arr[n]: the array to rotate
    Returns
    
    int[n]: the rotated array
    Input Format
    
    The first line contains two space-separated integers that denote , the number of integers, and , the number of left rotations to perform.
    The second line contains  space-separated integers that describe .
    
    Constraints
    
    Sample Input
    
    5 4
    1 2 3 4 5
    Sample Output
    
    5 1 2 3 4
    Explanation
    
    To perform  left rotations, the array undergoes the following sequence of changes:

---
+++ LeftRotation.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    array = []
+    k = d
+    for i in arr:
+        if k > 0:
+            k = k -1
+            continue
+        array.append(i)
+    for i in range(d):
+        array.append(arr[i])
+    return array
+    
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
